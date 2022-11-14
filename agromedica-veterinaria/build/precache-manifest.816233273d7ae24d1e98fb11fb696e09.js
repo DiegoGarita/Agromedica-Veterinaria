0967362a202afe2600a7376ae3afaf0f81e99943
Diego Garita <DiegoGarita2413@gmail.com>
2022-11-14 01:18:42 -0600

    build

---
+++ agromedica-veterinaria/build/precache-manifest.816233273d7ae24d1e98fb11fb696e09.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "506c83f05f8919540bc9",
+    "url": "/static/css/main.1fccced9.chunk.css"
+  },
+  {
+    "revision": "506c83f05f8919540bc9",
+    "url": "/static/js/main.506c83f0.chunk.js"
+  },
+  {
+    "revision": "c527d4adb695c818acb6",
+    "url": "/static/js/1.c527d4ad.chunk.js"
+  },
+  {
+    "revision": "a56c9aedca443afaffb0",
+    "url": "/static/css/2.240797f7.chunk.css"
+  },
+  {
+    "revision": "a56c9aedca443afaffb0",
+    "url": "/static/js/2.a56c9aed.chunk.js"
+  },
+  {
+    "revision": "bb7af62d3f8baf3a1520",
+    "url": "/static/js/runtime~main.bb7af62d.js"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "fda865aa153d3235749b41da3a1bc0ff",
+    "url": "/static/media/header-img.fda865aa.svg"
+  },
+  {
+    "revision": "8904ebce01232f09eaaacb2efa77068b",
+    "url": "/static/media/R1-F3.8904ebce.png"
+  },
+  {
+    "revision": "05379df7b6b9ae6a01d97bca9b4e3c24",
+    "url": "/static/media/R2-F5.05379df7.png"
+  },
+  {
+    "revision": "47550a2ce2b5e0632e8ed8f2df647b04",
+    "url": "/static/media/R4-F1.47550a2c.png"
+  },
+  {
+    "revision": "e75fa0275e6f0c353dad16e25d9e1b56",
+    "url": "/static/media/R6-F4.e75fa027.png"
+  },
+  {
+    "revision": "b67d676816c199b6305307688a2c9ac9",
+    "url": "/static/media/R7-F7.b67d6768.png"
+  },
+  {
+    "revision": "a0ae31e664c9ac180feef279c53fcda9",
+    "url": "/static/media/logo.a0ae31e6.jpg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "3bd41cbb98a79e1411f42735ca566176",
+    "url": "/static/media/accesorios-icon.3bd41cbb.png"
+  },
+  {
+    "revision": "11b2921a71fece3b7e3ad36f5aa47e3a",
+    "url": "/static/media/alimentos-icon.11b2921a.png"
+  },
+  {
+    "revision": "593db7c97fb7d543ca2dc03f94466f2b",
+    "url": "/static/media/farmacia-icon.593db7c9.png"
+  },
+  {
+    "revision": "7878058650a030f91e8cbeb7f8debc3f",
+    "url": "/static/media/farmacia2-icon.78780586.png"
+  },
+  {
+    "revision": "9f1d1e1028c0fd7d46781aab73326f5b",
+    "url": "/static/media/lecheria-icon.9f1d1e10.png"
+  },
+  {
+    "revision": "5614fda9689bbf6255a0d827f95c27c3",
+    "url": "/static/media/consulta-icon.5614fda9.png"
+  },
+  {
+    "revision": "5d7c30f60d116f5f2c7e4b277d2745c8",
+    "url": "/static/media/peluqueria-icon.5d7c30f6.png"
+  },
+  {
+    "revision": "04433dccf1d5713eba3c9b6e077491c0",
+    "url": "/static/media/A1.04433dcc.png"
+  },
+  {
+    "revision": "246b0319f681be9c0468e55bd812a75c",
+    "url": "/static/media/A2.246b0319.png"
+  },
+  {
+    "revision": "03f0b22908957480b228aa2252cb3a1a",
+    "url": "/static/media/A3.03f0b229.png"
+  },
+  {
+    "revision": "a6b5df0a9654a9b1757dedc694249184",
+    "url": "/static/media/A4.a6b5df0a.png"
+  },
+  {
+    "revision": "2d956216260370ce310c6ef7e6fe4709",
+    "url": "/static/media/A5.2d956216.png"
+  },
+  {
+    "revision": "5d841c7a84d4297d1153d2cadbc07e25",
+    "url": "/static/media/C1.5d841c7a.png"
+  },
+  {
+    "revision": "139c91f92c52200acb1d3c2fac142ecd",
+    "url": "/static/media/C2.139c91f9.jpg"
+  },
+  {
+    "revision": "6486669f4baac0a889a33c6029c912c6",
+    "url": "/static/media/C3.6486669f.jpg"
+  },
+  {
+    "revision": "823ee025019c9e17d0733cfb1b1086d9",
+    "url": "/static/media/C4.823ee025.jpg"
+  },
+  {
+    "revision": "af51539f3c44918bc601b218b8d4b8ab",
+    "url": "/static/media/C5.af51539f.jpg"
+  },
+  {
+    "revision": "b3102dd5c8f7352c58406763a641a3a0",
+    "url": "/static/media/C6.b3102dd5.jpg"
+  },
+  {
+    "revision": "8c4014dc0739e134935f89ec3ac25e70",
+    "url": "/static/media/C7.8c4014dc.jpg"
+  },
+  {
+    "revision": "18129a01ba5a7f1894465f229dc36397",
+    "url": "/static/media/C8.18129a01.png"
+  },
+  {
+    "revision": "3e3f9d7ca1da068bf0d5a7b9163f612c",
+    "url": "/static/media/C9.3e3f9d7c.png"
+  },
+  {
+    "revision": "b7301dbf2a78a5580f189225b625f0a8",
+    "url": "/static/media/R3.b7301dbf.png"
+  },
+  {
+    "revision": "629b216e54c2a54fe845add866416009",
+    "url": "/static/media/R5.629b216e.png"
+  },
+  {
+    "revision": "452db8063e9655a0027fa484d8805a6d",
+    "url": "/static/media/F2.452db806.png"
+  },
+  {
+    "revision": "bdde030cd8305be313588f011f7d3a79",
+    "url": "/static/media/F6.bdde030c.png"
+  },
+  {
+    "revision": "b302321a5998036f6caf69b6442099c1",
+    "url": "/static/media/G1.b302321a.png"
+  },
+  {
+    "revision": "170fe1f1e40aaf45ff0b0a095bc42ae3",
+    "url": "/static/media/G2.170fe1f1.png"
+  },
+  {
+    "revision": "35eeaf9fc5d6276f8e2e8c0249b47ea4",
+    "url": "/static/media/G3.35eeaf9f.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "582372fa36ffb20ae41a0dcf112d0a9d",
+    "url": "/static/media/banner-bg.582372fa.png"
+  },
+  {
+    "revision": "08bb6ae8f8d50476dad60aefec8687ed",
+    "url": "/index.html"
+  }
+];
